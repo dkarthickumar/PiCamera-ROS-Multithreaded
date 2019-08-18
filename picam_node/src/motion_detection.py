99c4a30b5c3d2e235cabffd3f39cf5cdc7d78f0b
dkarthickumar <dkarthick.kumar03@gmail.com>
2019-08-18 06:56:29 +0000

    Added background subtractor and motion detection logic

--- picam_node/src/motion_detection.py
+++ picam_node/src/motion_detection.py
@@ -4,6 +4,7 @@
 #
 import picam_pub as picam
 import cv2
+import sys
 
 class motion_detection():
     def __init__(self):
@@ -16,11 +17,19 @@ class motion_detection():
 	    try:
 	    	frame = self.vs.read()
 	     	fgmask = self.fgbg.apply(frame)
-	    	self.vs.pub_image(fgmask)
+		frame = cv2.bitwise_and(frame, frame, mask = fgmask) 
+		(_, cnts, _) = cv2.findContours(fgmask, cv2.RETR_EXTERNAL ,cv2.CHAIN_APPROX_SIMPLE)
+		for c in cnts:
+		    if cv2.contourArea(c) < 20:
+			continue
+		    (x,y,w,h) = cv2.boundingRect(c)
+		    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
+
+	    	self.vs.pub_image(frame)
 	    except KeyboardInterrupt:
     		print('interrupted!')
 		self.vs.stop()
-		break 
+		sys.exit() 
 	
 if __name__ == '__main__' : 
  	mot = motion_detection()
