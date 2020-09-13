9fcefa3ccdf2b3c88f48c7837014305c420df008
dkarthickumar <dkarthick.kumar03@gmail.com>
2020-09-13 12:18:34 +0000

    Added motor commands to stop and control all motor from same host machine for testing

--- picam_node/src/motor_cmd.py
+++ picam_node/src/motor_cmd.py
@@ -4,11 +4,16 @@ import os
 import time
 
 class motor_ctrl:
+
+    #
+    # controls the motro based on the rasp pi 
+    #
     def motor_cmd(self, angle_r_l, angle_u_d):
+
     	camera = os.environ["PI_CAMERA"]
-    	if (camera == "camera_left"):
+	if (camera == "camera_left"):
        	    start_byte = 'l'
-        elif (camera == "camera_right"):
+       	elif (camera == "camera_right"):
 	    start_byte = 'r'
     
         cmd = struct.pack('cbb', start_byte, angle_r_l, angle_u_d) 
@@ -20,6 +25,50 @@ class motor_ctrl:
 	    print('Nak')
 	if ret == 'A':
 	    print('Aak')
- 
+
+    def motor_stop(self):
+        cmd = struct.pack('cbb', 's', 0, 0) 
+        self.port.write(cmd)
+	ret = self.port.read_until(size=1)   
+	if ret == '':
+	    print('timed out')
+	if ret == 'N':
+	    print('Nak')
+	if ret == 'A':
+	    print('Aak')
+	
+
+    def motor_cmd_all(self,camera, angle_r_l, angle_u_d):
+
+	if (camera == "camera_left"):
+       	    start_byte = 'l'
+       	elif (camera == "camera_right"):
+	    start_byte = 'r'
+	if (camera == "camera_up_down"):
+       	    start_byte = 'u'
+	if (camera == "camera_stop"):
+       	    start_byte = 's'
+    
+        cmd = struct.pack('cbb', start_byte, angle_r_l, angle_u_d) 
+        self.port.write(cmd)
+	ret = self.port.read_until(size=1)   
+	if ret == '':
+	    print('timed out')
+	if ret == 'N':
+	    print('Nak')
+	if ret == 'A':
+	    print('Aak')
+
+
+    def get_motor_angles(self):
+	cmd = struct.pack('cbb', 'a', 0 ,0)
+	self.port.write(cmd)
+	if ret == '':
+	    print('timed out')
+	if ret == 'N':
+	    print('Nak')
+	if ret == 'A':
+	    print('Aak')
+	
     def __init__(self):
         self.port = serial.Serial("/dev/ttyUSB0",baudrate=115200, timeout=3.0)
--- picam_node/src/motor_ctrl.py
+++ picam_node/src/motor_ctrl.py
@@ -2,6 +2,7 @@ import sys
 from motor_cmd import motor_ctrl
 
 # Angle , speed, direction
-print( int(sys.argv[1]) , int(sys.argv[2]))
+print( sys.argv[1] , int(sys.argv[2]), int(sys.argv[3]))
 motor = motor_ctrl()
-motor.motor_cmd(int(sys.argv[1]), int(sys.argv[2]))
+
+motor.motor_cmd_all(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
