82b70f692b9783933cf625c21a5c3c9bb91df64e
dkarthickumar <dkarthick.kumar03@gmail.com>
2020-10-04 14:49:23 -0700

    Added hist callback to rective histogram

--- picam_node/src/color_object_detection.py
+++ picam_node/src/color_object_detection.py
@@ -6,37 +6,44 @@ import picam_pub as picam
 import cv2
 import sys
 from motor_cmd import motor_ctrl
+import rospy
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
 
 class color_object_detection():
     def __init__(self):
-	self.vs = picam.PiVideoStream(frame_size=(320,240), resolution=(1280, 720), framerate=30, ROS=True).start()
-	self.motor = motor_ctrl()
+        self.vs = picam.PiVideoStream(frame_size=(320, 240), resolution=(1280, 720), framerate=30, ROS=True).start()
+        self.motor = motor_ctrl()
 
+    def detect_object(self):
+        while True:
+            try:
+                frame = self.vs.read()
+                hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    def detect_object(self,roi_hist):
-	while True:
-	    try:
-	    	frame = self.vs.read()
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		
-		mask = cv2.calcBackProject([hsv],[0,1],roi_hist,[0,180,0,256],1)
- 		(_,cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		for c in cnts:
-		    if cv2.contourArea(c) < 20:
-			continue
-		    (x,y,w,h) = cv2.boundingRect(c)
-		    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
-		    
-	    	self.vs.pub_image(frame)
-	    except KeyboardInterrupt:
-    		print('interrupted!')
-		self.vs.stop()
-		sys.exit() 
+                mask = cv2.calcBackProject([hsv], [0, 1], roi_hist, [0, 180, 0, 256], 1)
+                (_, cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                for c in cnts:
+                    if cv2.contourArea(c) < 20:
+                        continue
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-if __name__ == '__main__' : 
- 	clr_obj_dect = color_object_detection()
-	img = clr_obj_dect.vs.read()
-	roi = img[120:130,130:140]
-	roi_hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
-	roi_hist = cv2.calcHist([hsv_roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
-	clr_obj_dect.detect_object(roi_hist)		
+                self.vs.pub_image(frame)
+            except KeyboardInterrupt:
+                print('interrupted!')
+                self.vs.stop()
+                sys.exit()
+
+    def get_hist_data(self,data):
+        print(data)
+
+if __name__ == '__main__':
+    clr_obj_dect = color_object_detection()
+    rospy.init_node('color_object_detect')
+    rospy.Subscriber('roi_hist_pub', numpy_msg(Floats), clr_obj_dect.get_hist_data)
+    # img = clr_obj_dect.vs.read()
+    # roi = img[120:130, 130:140]
+    # roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    # roi_hist = cv2.calcHist([hsv_roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    # clr_obj_dect.detect_object()
