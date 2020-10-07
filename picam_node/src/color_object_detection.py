a6d2c64839d98f4f18a94d8cbd83046bb9b5cd27
dkarthickumar <dkarthick.kumar03@gmail.com>
2020-10-06 22:33:09 -0700

    Added camshift algo to color obj detect

--- picam_node/src/color_object_detection.py
+++ picam_node/src/color_object_detection.py
@@ -14,21 +14,22 @@ class color_object_detection():
     def __init__(self):
         self.vs = picam.PiVideoStream(frame_size=(320, 240), resolution=(1280, 720), framerate=30, ROS=True).start()
         self.motor = motor_ctrl()
-	#pass
+        self.hist_ready = False
+        self.hist = []
+        self.term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
 
     def detect_object(self):
         while True:
             try:
                 frame = self.vs.read()
                 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-                mask = cv2.calcBackProject([hsv], [0, 1], roi_hist, [0, 180, 0, 256], 1)
-                (_, cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                for c in cnts:
-                    if cv2.contourArea(c) < 20:
-                        continue
-                    (x, y, w, h) = cv2.boundingRect(c)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if self.hist_ready:
+                    mask = cv2.calcBackProject([hsv], [0, 1], self.hist, [0, 180, 0, 256], 1)
+                    (_, cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    ret, track_window = cv.CamShift(dst, track_window, term_crit)
+                    pts = cv.boxPoints(ret)
+                    pts = np.int0(pts)
+                    frame = cv2.polylines(frame,[pts],True,255,2)
 
                 self.vs.pub_image(frame)
             except KeyboardInterrupt:
@@ -38,9 +39,10 @@ class color_object_detection():
 
     def get_hist_data(self,data):
         print('callback called')
-	#print(data.data) 
-	hist = np.asarray(data.data, 'float32').reshape(180,256)
-	print(hist)
+        self.hist = np.asarray(data.data, 'float32').reshape(180,256)
+        print(self.hist)
+        self.hist_ready = True
+
 if __name__ == '__main__':
     clr_obj_dect = color_object_detection()
     #rospy.init_node('color_object_detect')
